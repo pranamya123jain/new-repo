81727bdc01e73673acee1a725b931c67f4ab6a97
pranamya123jain <pranamya123jain@gmail.com>
2024-11-11 15:15:02 +0530

    commit1

---
+++ src/test/java/contactTest/CreateContactTest.java
@@ -0,0 +1,15 @@
+package contactTest;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	@Test
+	public void createCntactTest() {
+		System.out.println("Contact");
+	}
+
+	@Test
+	public void createWithContactNo() {
+		System.out.println("create contact with no123654");
+	}
+}
---
+++ src/test/java/orgTest/CreateOrgTest.java
@@ -0,0 +1,17 @@
+package orgTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+		@Test
+		public void creaOrgTest() {
+			System.out.println("Org");
+		}
+
+		@Test
+		public void createWithOrgNo() {
+			System.out.println("create Org with no123654");
+		}
+	}
+
+
