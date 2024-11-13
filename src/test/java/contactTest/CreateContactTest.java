0d1e7f2b729c2d45ad5aa077e3d1ddc751a13a51
pranamya123jain <pranamya123jain@gmail.com>
2024-11-13 10:22:33 +0530

    commit2

--- src/test/java/contactTest/CreateContactTest.java
+++ src/test/java/contactTest/CreateContactTest.java
@@ -12,5 +12,6 @@ public class CreateContactTest {
 	public void createWithContactNo() {
 		System.out.println("create contact with no123654");
 		System.out.println("12369");
+		System.out.println("======");
 	}
 }
